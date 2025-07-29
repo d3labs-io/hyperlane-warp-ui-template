7b538ad0aaa0af88687237aea82fcccefe5232da
Derryl <derryl@d3labs.io>
2025-07-29 16:37:36 +0700

    lint

--- src/components/tip/TipCard.tsx
+++ src/components/tip/TipCard.tsx
@@ -1,9 +1,6 @@
 import { IconButton, XCircleIcon } from '@hyperlane-xyz/widgets';
-import Image from 'next/image';
 import { useState } from 'react';
 import { config } from '../../consts/config';
-import { links } from '../../consts/links';
-import InfoCircle from '../../images/icons/info-circle.svg';
 import { Card } from '../layout/Card';
 
 export function TipCard() {
